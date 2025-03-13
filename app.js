17267abfadaf0c6f6aa9750f0ded9785b9b11a80
unknown <Santhiago_24771@ETEC.FURLAN>
2025-03-13 20:00:25 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+app.js 
+const express = require('express'); // importando o express
+const app = express(); // startando o express na variável app
+ 
+module.exports = app // exportando app para quem precisar usar
---
+++ server.js
@@ -0,0 +1,13 @@
+server.js 
+require('dotenv').config(); // importando o dotenv sem constante
+ 
+const app = require('./app'); // importando app.js
+const PORT = process.env.PORT || 3000; // pegando a porta do arquivo .env ou 3000 por padrão
+ 
+app.listen(
+    PORT, () => {
+        console.log(process.env.ISRUN);
+        console.log(`On port ${PORT}`);
+        console.log(`URL : http://localhost:${PORT}`);
+    }
+);
